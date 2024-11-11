50efc5a6733c3eb9ca03126047149d05b1c23690
Rafaela de Cassia Souza dos Santos 3D <00001074450103sp@al.educacao.sp.gov.br>
2024-11-11 20:25:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Quantos planetas Terra cabem dentro do Sol?',
+    'Um milhão. O Sol possui 1 392 700 km de diâmetro, medida 109 vezes maior que a Terra, que possui diâmetro de 12 742 km.    '
+)
+
+criaCartao(
+    'Geral',
+    'Em que lugar vivem mais cangurus do que pessoas?',
+    'Austrália. Na Austrália, a população de cangurus, cerca de 45 milhões, representa quase o dobro da população australiana (25 milhões). Ainda que isso seja interessante, o excesso de marsupiais no país pode afetar a biodiversidade.'
+)
+
+criaCartao(
+    'Geral',
+    'Quantos braços tem um polvo?',
+    'Oito. Os polvos são moluscos marinhos que possuem oito braços com ventosas.'
+)
+
+criaCartao(
+    'Geral',
+    'Em que ordem aparecem as cores do arco-íris sempre?',
+    'Vermelho, laranja, amarelo, verde, azul, anil (ou índigo) e violeta. O arco íris é um fenômeno físico natural que ocorre por meio da refração da luz branca do sol sobre as gotas de água.'
+)
